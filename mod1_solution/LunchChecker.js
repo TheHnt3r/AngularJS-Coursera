3ebd1047dd019afab3b1e65dc39ec98f043ba356
Hunter <Hunter@web.com>
2016-11-29 01:29:36 +0200

    assignment 1 final

--- mod1_solution/LunchChecker.js
+++ mod1_solution/LunchChecker.js
@@ -9,21 +9,30 @@ lunchController.$inject= ['$scope'];
 function lunchController($scope) {
 
   $scope.value= "";
-  $scope.checkLunch = function(){
-    $scope.lunch();
-  };
+  // $scope.checkLunch = function(){
+  //   return $scope.lunch();
+  // };
 
   $scope.lunch= function () {
-    var splitted= $scope.value.split(",")
-    splitted= splitted.filter(Boolean)
-    console.log(splitted);
-    if (splitted.length > 3) {
-        return "Too much";
-    } else {
-      return "Very Good!";
+      var splitted= $scope.value.split(",")
+      splitted= splitted.filter(Boolean)
+      //console.log(splitted);
+      switch (true) {
+        case (splitted.length > 3):
+          $scope.lunchOutput= "Too Much!";
+          break;
+        case (splitted.length <= 3 && splitted.length != 0):
+          $scope.lunchOutput= "That's Good";
+          break;
+        default:
+          if (splitted.length == 0) {
+            $scope.lunchOutput= "You've Got to Enter Something";
+            break;
+          }
+      }
+      //return
     }
-
-  };
+    //console.log($scope.lunch());
 
   };
 
